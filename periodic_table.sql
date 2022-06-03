0bb9d688fed541a7a75d6b650c6b1edfbd51b2dd
tiborg2 <rytis.cesna@gmail.com>
2022-06-03 11:11:54 +0000

    Full periodic elements DB

--- periodic_table.sql
+++ periodic_table.sql
@@ -126,8 +126,8 @@ INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
 INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
 INSERT INTO public.elements VALUES (1000, 'MT', 'moTanium');
-INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
+INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
 
 --
@@ -143,8 +143,8 @@ INSERT INTO public.properties VALUES (3, 'metal', 6.94, 180.54, 1342, 2);
 INSERT INTO public.properties VALUES (4, 'metal', 9.0122, 1287, 2470, 2);
 INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, 3);
 INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, 3);
+INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, -220, -188.1, 1);
 INSERT INTO public.properties VALUES (10, 'nonmetal', 20.18, -248.6, -246.1, 1);
-INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, 220.0, -188.1, 1);
 
 
 --
